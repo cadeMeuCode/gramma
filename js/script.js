4701bc0df9a7960194d176d57e700fa70563371c
Cade Meu Code <cademeucode@iMac-de-Abudu.local>
2023-02-28 23:01:13 +0900

    gramma - test version

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+ScrollReveal({ reset: true});
+
+ScrollReveal().reveal('.scroll-reveal-area', {distance: '250px', origin: 'bottom', duration: 1500});
+ScrollReveal().reveal('.footer-scroll-reveal', { delay: 200 } );
