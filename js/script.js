5b533aae9a9e8ab48c963b6d8420ebac05c43733
Cade Meu Code <cademeucode@iMac-de-Abudu.local>
2023-03-01 19:25:39 +0900

    adjusting module image size

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
 ScrollReveal({ reset: true});
 
-ScrollReveal().reveal('.scroll-reveal-area', {distance: '250px', origin: 'bottom', duration: 1500});
+ScrollReveal().reveal('.scroll-reveal-area', {distance: '250px', origin: 'bottom', duration: 1800});
 ScrollReveal().reveal('.footer-scroll-reveal', { delay: 200 } );
